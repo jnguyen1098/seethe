432abbb90c52ca4de14325298cbc83835f86f28d
jnguyen1098 <jnguyen1098@gmail.com>
2021-02-06 00:51:36 -0500

    start __func__ support

--- seethe.h
+++ seethe.h
@@ -20,7 +20,7 @@
 #include <stdio.h>
 #include <time.h>
 
-#define emit_log(colour, level, file, line, ...) do {                           \
+#define emit_log(colour, level, file, func, line, ...) do {                     \
     time_t raw_time;                                                            \
     time(&raw_time);                                                            \
     char time_buffer[80];                                                       \
@@ -38,32 +38,49 @@
 #define CRITICAL        5
 
 #define debug(...) do {                                                         \
-    if (LOG_LEVEL == DEBUG)                                                     \
-        emit_log(DEBUG_COLOUR, "DEBUG", __FILE__, __LINE__, __VA_ARGS__);       \
+    if (LOG_LEVEL == DEBUG) {                                                   \
+        emit_log(                                                               \
+            DEBUG_COLOUR, "DEBUG", __FILE__, __func__, __LINE__, __VA_ARGS__    \
+        );                                                                      \
+    }                                                                           \
 } while (0)
 
 #define info(...) do {                                                          \
-    if (LOG_LEVEL <= INFO)                                                      \
-        emit_log(INFO_COLOUR, "INFO", __FILE__, __LINE__, __VA_ARGS__);         \
+    if (LOG_LEVEL <= INFO) {                                                    \
+        emit_log(                                                               \
+            INFO_COLOUR, "INFO", __FILE__, __func__, __LINE__, __VA_ARGS__      \
+        );                                                                      \
+    }                                                                           \
 } while (0)
 
 #define notice(...) do {                                                        \
-    if (LOG_LEVEL <= NOTICE)                                                    \
-        emit_log(NOTICE_COLOUR, "NOTICE", __FILE__, __LINE__, __VA_ARGS__);     \
+    if (LOG_LEVEL <= NOTICE) {                                                  \
+        emit_log(                                                               \
+            NOTICE_COLOUR, "NOTICE", __FILE__, __func__, __LINE__, __VA_ARGS__  \
+        );                                                                      \
+    }                                                                           \
 } while (0)
 
 #define warning(...) do {                                                       \
-    if (LOG_LEVEL <= WARNING)                                                   \
-        emit_log(WARNING_COLOUR, "WARNING", __FILE__, __LINE__, __VA_ARGS__);   \
+    if (LOG_LEVEL <= WARNING) {                                                 \
+        emit_log(                                                               \
+            WARNING_COLOUR, "WARNING", __FILE__, __func__, __LINE__, __VA_ARGS__\
+        );                                                                      \
+    }                                                                           \
 } while (0)
 
 #define error(...) do {                                                         \
-    if (LOG_LEVEL <= ERROR)                                                     \
-        emit_log(ERROR_COLOUR, "ERROR", __FILE__, __LINE__, __VA_ARGS__);       \
+    if (LOG_LEVEL <= ERROR) {                                                   \
+        emit_log(                                                               \
+            ERROR_COLOUR, "ERROR", __FILE__, __func__, __LINE__, __VA_ARGS__    \
+        );                                                                      \
+    }                                                                           \
 } while (0)
 
 #define critical(...) do {                                                      \
-    emit_log(CRITICAL_COLOUR, "CRITICAL", __FILE__, __LINE__, __VA_ARGS__);     \
+    emit_log(                                                                   \
+        CRITICAL_COLOUR, "CRITICAL", __FILE__, __func__, __LINE__, __VA_ARGS__  \
+    );                                                                          \
 } while (0)
 
 #endif // seethe.h
